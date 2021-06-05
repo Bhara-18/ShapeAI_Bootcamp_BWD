4c66f5a20b84fa9241f670ff0456c61e10dce98b
Bhara-18 <84090285+Bhara-18@users.noreply.github.com>
2021-06-06 02:34:19 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return( 
+   <div className = "note">
+     <h1> Javascript and React.JS  </h1>
+     <p>
+       This was an awesome class and im
+        forever grateful that i recieved such an 
+        enriching course in my life 
+     </p>
+   </div>
+  )
+};
+
+export default Note;
